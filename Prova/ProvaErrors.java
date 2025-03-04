4dd301c50513580d79fa1ae5aff961b306ddad75
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-04 16:50:12 +0100

    Create ProvaErrors.java

---
+++ Prova/ProvaErrors.java
@@ -0,0 +1,12 @@
+public class ProvaErrors {
+    public static void main(String[] args) {
+        int[] myNums = { 1, 2, 3, 4 };
+        // System.out.println(myNums[12]); //Generà un errore
+
+        try {
+            System.out.println(myNums[12]); 
+        } catch (Exception e) {
+            System.out.println("Errore, qualcosa è andato storto");
+        }
+    }
+}
